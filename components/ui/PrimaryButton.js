22767288d0783d2e0fd8807357d77c469204ad39
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-18 17:39:45 +0000

    moved Title and Custom button component to ui folder

---
+++ components/ui/PrimaryButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Colors from "../../constants/Colors";
+
+const PrimaryButton = ({ children, onPress }) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+export default PrimaryButton;
