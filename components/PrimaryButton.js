f195dd88c90fc8c312234211cca49a461b2ea2f6
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-18 13:40:18 +0000

    defined custom Primary button

---
+++ components/PrimaryButton.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={() => {
+          console.log("hi");
+        }}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+export default PrimaryButton;
