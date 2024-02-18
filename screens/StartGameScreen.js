87fe5463f44260c8faf3c445b381dac7781911de
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-18 14:23:16 +0000

    receiev prop function to trigger if a valid number has been entered too change screen in parent component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Alert, StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState("");
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -20,7 +20,7 @@ const StartGameScreen = () => {
       );
       return;
     }
-    console.log("Valid Number");
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.inputContainer}>
